105953f7a54789ef97407f71fcbbab62014c6bbb
Han Wang <hanforwork896@gmail.com>
2024-12-14 15:06:02 +0800

    feat: add scss files & images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  // nitro: {
-  //   preset: 'render_com'
-  // },
+  nitro: {
+    preset: 'render_com'
+  },
   css: ['@/assets/styles/all.scss'],
   modules: ['@nuxt/icon', '@samk-dev/nuxt-vcalendar'],
 })
